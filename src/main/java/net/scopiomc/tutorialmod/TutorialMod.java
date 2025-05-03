5bb9c90aa2d7b12c5a6fd6cbbfd710dd8086651e
scopiomc25 <scopiomc5@gmail.com>
2025-05-04 00:55:41 +0200

    Raw Sapphire Item hinzugefügt

--- src/main/java/net/scopiomc/tutorialmod/TutorialMod.java
+++ src/main/java/net/scopiomc/tutorialmod/TutorialMod.java
@@ -44,7 +44,8 @@ public class TutorialMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
-            event.accept((ModItems.SAPPHIRE));
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
         }
     }
 

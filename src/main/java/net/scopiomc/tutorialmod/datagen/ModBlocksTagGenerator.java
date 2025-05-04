861ede1e77e7dbf33a197f1052786022f1c253d8
scopiomc25 <scopiomc5@gmail.com>
2025-05-04 17:44:26 +0200

    Add data generators for recipes, loot tables, and tags
    Introduced multiple data generator classes to streamline the creation of recipes, item models, block states, loot tables, and tags. These additions improve maintainability and automate data generation for new blocks, items, and associated metadata.

---
+++ src/main/java/net/scopiomc/tutorialmod/datagen/ModBlocksTagGenerator.java
@@ -0,0 +1,57 @@
+package net.scopiomc.tutorialmod.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.scopiomc.tutorialmod.TutorialMod;
+import net.scopiomc.tutorialmod.block.ModBlocks;
+import net.scopiomc.tutorialmod.util.ModTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlocksTagGenerator extends BlockTagsProvider {
+
+
+    public ModBlocksTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, TutorialMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
+                .add(ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                        ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                        ModBlocks.END_STONE_SAPPHIRE_ORE.get())
+                .addTag(Tags.Blocks.ORES);
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                        ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                        ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+        this.tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(ModBlocks.SOUND_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_STONE_TOOL)
+                .add(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+        this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL);
+    }
+}
---
+++ src/main/java/net/scopiomc/tutorialmod/datagen/ModRecipeProvider.java
@@ -0,0 +1,95 @@
+package net.scopiomc.tutorialmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.scopiomc.tutorialmod.TutorialMod;
+import net.scopiomc.tutorialmod.block.ModBlocks;
+import net.scopiomc.tutorialmod.item.ModItems;
+import net.scopiomc.tutorialmod.util.ModTags;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    private static final List<ItemLike> SAPPHIRE_SMELTABLES = List.of(ModItems.RAW_SAPPHIRE.get(),
+            ModBlocks.SAPPHIRE_ORE.get(), ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(), ModBlocks.NETHER_SAPPHIRE_ORE.get(), ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+    private static final List<ItemLike> RAW_SAPPHIRE_BLOCK_SMELTING = List.of(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+
+    public ModRecipeProvider(PackOutput pOutput) {
+        super(pOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        oreBlasting(pWriter, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.7F, 50, "blasting");
+        oreSmelting(pWriter, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.3F, 100, "smelting");
+        oreSmelting(pWriter, RAW_SAPPHIRE_BLOCK_SMELTING, RecipeCategory.MISC, ModBlocks.SAPPHIRE_BLOCK.get(), 0.3F*12, 100*7, "smelting");
+        oreBlasting(pWriter, RAW_SAPPHIRE_BLOCK_SMELTING , RecipeCategory.MISC, ModBlocks.SAPPHIRE_BLOCK.get(), 0.7F*12, 50*7, "blasting");
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.SAPPHIRE_BLOCK.get())
+                .pattern("SSS")
+                .pattern("SSS")
+                .pattern("SSS")
+                .define('S', ModItems.SAPPHIRE.get())
+                .unlockedBy(getHasName(ModItems.SAPPHIRE.get()), has(ModItems.SAPPHIRE.get()))
+                .save(pWriter);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.RAW_SAPPHIRE_BLOCK.get())
+                .pattern("RRR")
+                .pattern("RRR")
+                .pattern("RRR")
+                .define('R', ModItems.RAW_SAPPHIRE.get())
+                .unlockedBy(getHasName(ModItems.RAW_SAPPHIRE.get()), has(ModItems.RAW_SAPPHIRE.get()))
+                .save(pWriter);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.SOUND_BLOCK.get())
+                .pattern("SSS")
+                .pattern("SNS")
+                .pattern("SSS")
+                .define('S', ModItems.SAPPHIRE.get())
+                .define('N', Blocks.NOTE_BLOCK)
+                .unlockedBy(getHasName(ModItems.SAPPHIRE.get()), has(ModItems.SAPPHIRE.get()))
+                .save(pWriter);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 9).requires(ModBlocks.SAPPHIRE_BLOCK.get())
+                .unlockedBy(getHasName(ModBlocks.SAPPHIRE_BLOCK.get()), has(ModBlocks.SAPPHIRE_BLOCK.get()))
+                .save(pWriter);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.RAW_SAPPHIRE.get(), 9).requires(ModBlocks.RAW_SAPPHIRE_BLOCK.get())
+                .unlockedBy(getHasName(ModBlocks.RAW_SAPPHIRE_BLOCK.get()), has(ModBlocks.RAW_SAPPHIRE_BLOCK.get()))
+                .save(pWriter);
+    }
+
+    protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients,
+                                      RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
+
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE,
+                pIngredients, pCategory, pResult, pExperience,
+                pCookingTIme, pGroup, "_from_smelting");
+    }
+
+    protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients,
+                                      RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE,
+                pIngredients, pCategory, pResult, pExperience,
+                pCookingTime, pGroup, "_from_blasting");
+    }
+
+    protected static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer,
+                                     RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer,
+                                     List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                     float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+
+        for(ItemLike itemlike : pIngredients) {
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory,
+                    pResult, pExperience, pCookingTime, pCookingSerializer)
+                    .group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(pFinishedRecipeConsumer, TutorialMod.MODID + ":" +getItemName(pResult)
+                            + pRecipeName + "_" + getItemName(itemlike));
+        }
+
+    }
+}
